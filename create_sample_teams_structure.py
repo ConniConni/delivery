9e1b3fdde42e00389b3e56b5e4d4bbdefb081ec9
ConniConni <nmtgktmo@gmail.com>
2025-10-12 21:32:42 +0900

    configの値の保持の仕方を確認(戻し)

--- create_sample_teams_structure.py
+++ create_sample_teams_structure.py
@@ -46,11 +46,6 @@ def create_sample_teams_structure(config):
     # KeyErrorが発生しないよう、get()メソッドを使用するか、事前にセクションとキーの存在を確認
     try:
         sample_teams_root = Path(config["Paths"]["sample_teams_root"])
-        print("*" * 10)
-        print(f"sample_teams_root:{sample_teams_root}")
-        print(f'Paths:{config["Paths"]}')
-        print(f'sample_teams_root:{config["Paths"]["sample_teams_root"]}')
-        print("*" * 10)
         project_name = config["Project"]["project_name"]
         item_name = config["Project"]["item_name"]
     except KeyError as e:
