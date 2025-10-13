3544d7cfc9fc4a250b2ecdb178bc0f97acd02e04
ConniConni <nmtgktmo@gmail.com>
2025-10-13 12:58:50 +0900

    TYPE_FILE選択のエラーハンドリングを追加

--- create_sample_teams_structure.py
+++ create_sample_teams_structure.py
@@ -24,8 +24,8 @@ PROCESS_MAP = {
 # ファイル種別
 FILE_TYPE = {
     "1": "メイン資料",
-    "2": "セルフチェック表",
-    "3": "レビュー記録表",
+    "2": "レビュー記録表",
+    "3": "セルフチェック表",
 }
 
 
@@ -39,11 +39,21 @@ def load_config(config_file="config.ini"):
 def create_empty_excel_file(type, title, project_name, item_name, file_path: Path):
     """空のExcelファイルを模したダミーファイルを作成する"""
     try:
+        if not isinstance(type, str):
+            raise TypeError("ファイル種別：typeは文字列で指定してください。")
+
+        if type not in FILE_TYPE:
+            raise ValueError(f"無効なファイル種別です: {type}")
+
+    except (TypeError, ValueError) as e:
+        logging.error(f"エラー: {e}")
+
+    try:
         wb = Workbook()
         ws = wb.active
         # ws["A1"] = f"これはダミーのExcelファイルです。ファイル名: {file_path.name}"
         # ws.cell(row=1, column=1, value=f"これはダミーのExcelファイルです。ファイル名: {file_path.name}")
-        # 表紙タイトルチェックのために、特定のセルに値を設定することも可能
+
         if type == 1:
             ws["B5"] = f"{title}¥n{project_name}¥n{item_name}"
             # B5セルの書式を変更し、テキストの折り返しを有効にする。
@@ -177,99 +187,101 @@ def create_sample_teams_structure(config):
 
         # 成果物/レビューフォルダ構造
         # 050.製造はレビューのみ
-        if p_num != "050" or p_num == "050":  # 全ての工程で成果物フォルダを作成
-            results_dir = process_dir / "成果物"
-            results_dir.mkdir(exist_ok=True)
+        type_review = 2
 
-            # 内部レビュー
-            internal_review_dir = results_dir / "内部レビュー"
-            internal_review_dir.mkdir(exist_ok=True)
+        # if p_num != "050" or p_num == "050":  # 全ての工程で成果物フォルダを作成
+        #     results_dir = process_dir / "成果物"
+        #     results_dir.mkdir(exist_ok=True)
 
-            date_folder_int1 = internal_review_dir / date1.strftime("%Y%m%d")
-            date_folder_int1.mkdir(exist_ok=True)
-            # if (
-            #     main_excel_file_path and p_num != "090"
-            # ):  # 090はレビューフォルダにも複数のファイルがくる
-            #     create_empty_excel_file(date_folder_int1 / main_excel_file_path.name)
-            # elif p_num == "090":
-            #     create_empty_excel_file(
-            #         date_folder_int1 / f"結合試験成績書_{project_name}_{item_name}.xlsx"
-            #     )
-            #     create_empty_excel_file(
-            #         date_folder_int1 / f"試験結果報告書_{project_name}_{item_name}.xlsx"
-            #     )
+        #     # 内部レビュー
+        #     internal_review_dir = results_dir / "内部レビュー"
+        #     internal_review_dir.mkdir(exist_ok=True)
 
-            # create_empty_excel_file(
-            #     date_folder_int1
-            #     / f"レビューチェックリスト_{p_num}_社内_1回目_{project_name}_{item_name}.xlsx"
-            # )
-            # create_empty_excel_file(
-            #     date_folder_int1
-            #     / f"レビュー記録表_{p_name}_社内_1回目_{project_name}_{item_name}.xlsx"
-            # )
+        #     date_folder_int1 = internal_review_dir / date1.strftime("%Y%m%d")
+        #     date_folder_int1.mkdir(exist_ok=True)
+        #     if (
+        #         main_excel_file_path and p_num != "090"
+        #     ):  # 090はレビューフォルダにも複数のファイルがくる
+        #         create_empty_excel_file(date_folder_int1 / main_excel_file_path.name)
+        #     elif p_num == "090":
+        #         create_empty_excel_file(
+        #             date_folder_int1 / f"結合試験成績書_{project_name}_{item_name}.xlsx"
+        #         )
+        #         create_empty_excel_file(
+        #             date_folder_int1 / f"試験結果報告書_{project_name}_{item_name}.xlsx"
+        #         )
 
-            # 030に2回目のレビューを追加
-            # if p_num == "030":
-            #     date_folder_int2 = internal_review_dir / date2.strftime("%Y%m%d")
-            #     date_folder_int2.mkdir(exist_ok=True)
-            #     create_empty_excel_file(
-            #         date_folder_int2 / f"調査検討書_{project_name}_{item_name}.xlsx"
-            #     )
-            #     create_empty_excel_file(
-            #         date_folder_int2
-            #         / f"レビューチェックリスト_{p_num}_社内_2回目_{project_name}_{item_name}.xlsx"
-            #     )
-            #     create_empty_excel_file(
-            #         date_folder_int2
-            #         / f"レビュー記録表_{p_name}_社内_2回目_{project_name}_{item_name}.xlsx"
-            #     )
+        #     create_empty_excel_file(
+        #         date_folder_int1
+        #         / f"レビューチェックリスト_{p_num}_社内_1回目_{project_name}_{item_name}.xlsx"
+        #     )
+        #     create_empty_excel_file(
+        #         date_folder_int1
+        #         / f"レビュー記録表_{p_name}_社内_1回目_{project_name}_{item_name}.xlsx"
+        #     )
 
-            # 外部レビュー
-            external_review_dir = results_dir / "外部レビュー"
-            external_review_dir.mkdir(exist_ok=True)
+        # 030に2回目のレビューを追加
+        # if p_num == "030":
+        #     date_folder_int2 = internal_review_dir / date2.strftime("%Y%m%d")
+        #     date_folder_int2.mkdir(exist_ok=True)
+        #     create_empty_excel_file(
+        #         date_folder_int2 / f"調査検討書_{project_name}_{item_name}.xlsx"
+        #     )
+        #     create_empty_excel_file(
+        #         date_folder_int2
+        #         / f"レビューチェックリスト_{p_num}_社内_2回目_{project_name}_{item_name}.xlsx"
+        #     )
+        #     create_empty_excel_file(
+        #         date_folder_int2
+        #         / f"レビュー記録表_{p_name}_社内_2回目_{project_name}_{item_name}.xlsx"
+        #     )
 
-            # 外部レビューがある工程のみ作成
-            # if p_num in ["030", "040", "080", "090"]:
-            #     date_folder_ext1 = external_review_dir / date3.strftime("%Y%m%d")
-            #     date_folder_ext1.mkdir(exist_ok=True)
-            #     if main_excel_file_path and p_num != "090":
-            #         create_empty_excel_file(
-            #             date_folder_ext1 / main_excel_file_path.name
-            #         )
-            #     elif p_num == "090":
-            #         create_empty_excel_file(
-            #             date_folder_ext1
-            #             / f"結合試験成績書_{project_name}_{item_name}.xlsx"
-            #         )
-            #         create_empty_excel_file(
-            #             date_folder_ext1
-            #             / f"試験結果報告書_{project_name}_{item_name}.xlsx"
-            #         )
-            #     create_empty_excel_file(
-            #         date_folder_ext1
-            #         / f"レビューチェックリスト_{p_num}_社外_1回目_{project_name}_{item_name}.xlsx"
-            #     )
-            #     create_empty_excel_file(
-            #         date_folder_ext1
-            #         / f"レビュー記録表_{p_name}_社外_1回目_{project_name}_{item_name}.xlsx"
-            #     )
+        # 外部レビュー
+        # external_review_dir = results_dir / "外部レビュー"
+        # external_review_dir.mkdir(exist_ok=True)
 
-            #     # 030に2回目の外部レビューを追加
-            #     if p_num == "030":
-            #         date_folder_ext2 = external_review_dir / date4.strftime("%Y%m%d")
-            #         date_folder_ext2.mkdir(exist_ok=True)
-            #         # 故意にファイル名を少し変えて、フェーズ1の収集ロジックのテストに使用
-            #         create_empty_excel_file(
-            #             date_folder_ext2 / f"調査B_{project_name}_{item_name}.xlsx"
-            #         )
-            #         create_empty_excel_file(
-            #             date_folder_ext2
-            #             / f"レビューチェックリスト_{p_num}_社外_2回目_{project_name}_{item_name}.xlsx"
-            #         )
-            #         create_empty_excel_file(
-            #             date_folder_ext2
-            #             / f"レビュー記録表_{p_name}_社外_2回目_{project_name}_{item_name}.xlsx"
-            #         )
+        # 外部レビューがある工程のみ作成
+        # if p_num in ["030", "040", "080", "090"]:
+        #     date_folder_ext1 = external_review_dir / date3.strftime("%Y%m%d")
+        #     date_folder_ext1.mkdir(exist_ok=True)
+        #     if main_excel_file_path and p_num != "090":
+        #         create_empty_excel_file(
+        #             date_folder_ext1 / main_excel_file_path.name
+        #         )
+        #     elif p_num == "090":
+        #         create_empty_excel_file(
+        #             date_folder_ext1
+        #             / f"結合試験成績書_{project_name}_{item_name}.xlsx"
+        #         )
+        #         create_empty_excel_file(
+        #             date_folder_ext1
+        #             / f"試験結果報告書_{project_name}_{item_name}.xlsx"
+        #         )
+        #     create_empty_excel_file(
+        #         date_folder_ext1
+        #         / f"レビューチェックリスト_{p_num}_社外_1回目_{project_name}_{item_name}.xlsx"
+        #     )
+        #     create_empty_excel_file(
+        #         date_folder_ext1
+        #         / f"レビュー記録表_{p_name}_社外_1回目_{project_name}_{item_name}.xlsx"
+        #     )
+
+        #     # 030に2回目の外部レビューを追加
+        #     if p_num == "030":
+        #         date_folder_ext2 = external_review_dir / date4.strftime("%Y%m%d")
+        #         date_folder_ext2.mkdir(exist_ok=True)
+        #         # 故意にファイル名を少し変えて、フェーズ1の収集ロジックのテストに使用
+        #         create_empty_excel_file(
+        #             date_folder_ext2 / f"調査B_{project_name}_{item_name}.xlsx"
+        #         )
+        #         create_empty_excel_file(
+        #             date_folder_ext2
+        #             / f"レビューチェックリスト_{p_num}_社外_2回目_{project_name}_{item_name}.xlsx"
+        #         )
+        #         create_empty_excel_file(
+        #             date_folder_ext2
+        #             / f"レビュー記録表_{p_name}_社外_2回目_{project_name}_{item_name}.xlsx"
+        #         )
 
     logging.info("サンプルTeamsフォルダ構造の生成が完了しました。")
 
